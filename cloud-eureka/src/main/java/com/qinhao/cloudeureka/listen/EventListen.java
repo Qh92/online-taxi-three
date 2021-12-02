b56f0123b8727b47ff36d664caea39b68263f686
Qh92 <771600967@qq.com>
2021-12-02 22:54:27 +0800

    feat:添加配置文件

--- cloud-eureka/src/main/java/com/qinhao/cloudeureka/listen/EventListen.java
+++ cloud-eureka/src/main/java/com/qinhao/cloudeureka/listen/EventListen.java
@@ -1,6 +1,7 @@
 package com.qinhao.cloudeureka.listen;
 
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EventListen {
 
+    @EventListener
     public void listen(EurekaInstanceCanceledEvent event){
         // 模拟发邮件或短信
         System.out.println("服务下线: " + event.getServerId());
